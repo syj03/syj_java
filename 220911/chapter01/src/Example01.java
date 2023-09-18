07c31a6096206d8fe34658e90103997157fc2170
SeoYeongJin <seoyjwork@gmail.com>
2023-09-18 11:27:54 +0900

    first code

---
+++ 220911/chapter01/src/Example01.java
@@ -0,0 +1,16 @@
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		final int SPEED =20; 
+		int count =  10;
+		//SPEED = 40;
+		count = 11;
+		System.out.print("상수 SPEED 값은");
+		//System.out.println(SPEED);
+		System.out.print("상수 count 값은");
+		System.out.println(count);
+
+	}
+
+}
---
+++ 220911/chapter02/src/chapter02/variable.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class variable {
+
+	public static void main(String[] args) {
+		int days = 10;
+		System.out.println(days);
+
+		int seeds = 20;
+		System.out.println(seeds);
+
+	}
+
+}
